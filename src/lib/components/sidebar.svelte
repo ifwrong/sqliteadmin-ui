2a7c0a882d69b0093cc5d721390fdb54387a734c
Joel Sequeira <joelseq96@gmail.com>
2025-01-26 00:35:51 -0500

    Add collapsible view for tables

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import Button from '$lib/components/ui/button/button.svelte'
+  import * as Collapsible from '$lib/components/ui/collapsible'
+  import { ChevronDown, ChevronUp } from 'lucide-svelte'
 
   export const {
     tables,
@@ -10,21 +12,40 @@
     onSelect: (table: string) => void
     selectedTable: string
   } = $props()
+
+  let isTablesExpanded = $state(false)
 </script>
 
 <div class="sticky pb-12">
   <div class="space-y-4 py-4">
     <div class="px-3 py-2">
-      <h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">Tables</h2>
-      <div class="space-y-1">
-        {#each tables as table}
+      <Collapsible.Root bind:open={isTablesExpanded}>
+        <Collapsible.Trigger asChild let:builder>
           <Button
-            variant={table === selectedTable ? 'default' : 'ghost'}
-            class="w-full justify-start"
-            on:click={() => onSelect(table)}>{table}</Button
+            builders={[builder]}
+            variant="ghost"
+            class="w-full p-2 flex justify-between"
           >
-        {/each}
-      </div>
+            <h2 class="text-lg font-semibold tracking-tight">Tables</h2>
+            {#if isTablesExpanded}
+              <ChevronUp class="h-4 w-4" />
+            {:else}
+              <ChevronDown class="h-4 w-4" />
+            {/if}
+          </Button>
+        </Collapsible.Trigger>
+        <Collapsible.Content>
+          <div class="space-y-1 ml-4">
+            {#each tables as table}
+              <Button
+                variant={table === selectedTable ? 'default' : 'ghost'}
+                class="w-full justify-start"
+                on:click={() => onSelect(table)}>{table}</Button
+              >
+            {/each}
+          </div>
+        </Collapsible.Content>
+      </Collapsible.Root>
     </div>
   </div>
 </div>
