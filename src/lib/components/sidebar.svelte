5636daaa213ee5beb7a8b28639680286c6efaa7e
Joel Sequeira <joelseq96@gmail.com>
2025-01-26 15:13:42 -0500

    Fix ts errors in sidebar.svelte

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -3,29 +3,25 @@
   import * as Collapsible from '$lib/components/ui/collapsible'
   import { ChevronDown, ChevronUp } from 'lucide-svelte'
 
-  export const {
+  const {
     tables,
     onSelect,
     selectedTable,
   }: {
     tables: string[]
     onSelect: (table: string) => void
-    selectedTable: string
+    selectedTable: string | undefined
   } = $props()
 
-  let isTablesExpanded = $state(false)
+  let isTablesExpanded = $state(true)
 </script>
 
 <div class="sticky pb-12 overflow-y-auto h-[calc(100vh-72px)]">
   <div class="space-y-4 py-4">
     <div class="px-3 py-2">
       <Collapsible.Root bind:open={isTablesExpanded}>
-        <Collapsible.Trigger asChild let:builder>
-          <Button
-            builders={[builder]}
-            variant="ghost"
-            class="w-full p-2 flex justify-between"
-          >
+        <Collapsible.Trigger class="w-full">
+          <Button variant="ghost" class="w-full p-2 flex justify-between">
             <h2 class="font-semibold tracking-tight">Tables</h2>
             {#if isTablesExpanded}
               <ChevronUp class="h-4 w-4" />
@@ -40,7 +36,7 @@
               <Button
                 variant={table === selectedTable ? 'default' : 'ghost'}
                 class="w-full justify-start"
-                on:click={() => onSelect(table)}>{table}</Button
+                onclick={() => onSelect(table)}>{table}</Button
               >
             {/each}
           </div>
